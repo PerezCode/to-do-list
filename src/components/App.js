afa24895ef6aa6b487a3159f009c357894a36aa5
David Perez <dperezval08@gmail.com>
2020-03-24 18:53:58 -0500

    State management

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import './styles/App.css';
 
-function App() {
-  return (
-    <div className="container">
-      <h1 className="title">
-        Hello world <span aria-label="emoji" role="img">🔥</span>
-      </h1>
-      <input type="text" className="new-task"/>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = { newTask: ""}
+  }
+
+  handleChange = (e) => {
+    this.setState({newTask: e.target.value});
+  }
+
+  render() {
+    return(
+      <div className="container">
+        <h1 className="title">
+          Hello world <span aria-label="emoji" role="img">🔥</span>
+        </h1>
+        <input type="text" onChange={this.handleChange} className="new-task"/>
+        <h2 className="test-label">{this.state.newTask}</h2>
+      </div>
+    )
+  }
 }
 
 export default App;
