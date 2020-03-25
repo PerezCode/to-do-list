c5a17ae7ce0aa778a468af54e2f4d664fdcf968f
David Perez <dperezval08@gmail.com>
2020-03-25 18:52:42 -0500

    Component iteration

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,44 @@ class App extends React.Component {
 
   constructor(props){
     super(props);
-    this.state = { newTask: ""}
+    this.state = {
+      newTask: "",
+      tasks: [],
+    }
   }
 
   handleChange = (e) => {
     this.setState({newTask: e.target.value});
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let currentTask = this.state.tasks;
+    currentTask.push(this.state.newTask);
+    this.setState({
+      newTask: "",
+      tasks: currentTask,
+    })
+  }
+
   render() {
-    return(
+    return (
       <div className="container">
         <h1 className="title">
           Hello world <span aria-label="emoji" role="img">🔥</span>
         </h1>
-        <input type="text" onChange={this.handleChange} className="new-task"/>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text"value={this.state.newTask} onChange={this.handleChange} className="new-task"/>
+        </form>
         <h2 className="test-label">{this.state.newTask}</h2>
+        {
+          this.state.tasks.map( task => (
+            <div className="task-container">
+              <h3 className="task">{task}</h3>
+            </div>
+            )
+          )
+        }
       </div>
     )
   }
