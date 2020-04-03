1440d0c2db356f6223a435cceee1431b6206c173
David Perez <dperezval08@gmail.com>
2020-04-02 22:27:12 -0500

    Improved delete button

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,13 +3,21 @@ import React from "react";
 const Task = props => {
   return (
     <div className="task-container" key={props.id}>
-      <h3 className="task">{props.value}</h3>
-      <button
-        onClick={() => {props.handleDelete(props.id)}}
-        className="delete-button"
-      >
-      X
-      </button>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 pr-0">
+            <h3 className="task">{props.value}</h3>
+          </div>
+          <div className="col-2 p-0 d-flex justify-content-end">
+            <button
+              onClick={() => {props.handleDelete(props.id)}}
+              className="btn btn-danger mt-2 mr-2 delete-button"
+            >
+            X
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
